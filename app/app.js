2f76d5230b44fa3a178206a023085c38e2638742
Zoran Panev <panev.zoran.te@gmail.com>
2017-07-23 17:20:48 +0200

    First commit

---
+++ app/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module("movieDB", [
+	'ngRoute',
+	'movieDB.catalog',
+	'movieDB.movie',
+	'movieDB.genre',
+	'movieDB.paginate-filter',
+	'movieDB.movie-service'
+	]).
+config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+		when("/", {templateUrl: "catalog/catalog.html", controller: "catalogCtrl"}).
+		when("/movie/:id", {templateUrl: "movie/movie.html", controller: "movieCtrl"}).
+		when("/genre/:genreName", {templateUrl: "catalog/catalog.html", controller: "genreCtrl"}).
+    	when("", {templateUrl: "catalog/catalog.html", controller: "catalogCtrl"}).
+		otherwise({redirectTo: "/"});
+}]);
